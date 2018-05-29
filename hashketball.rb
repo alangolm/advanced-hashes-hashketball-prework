a5617a5f04ddc4b8ffe873dd629d4381d82a2ba0
Alan Golman <golman.alan@gmail.com>
2018-05-29 19:08:10 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -195,6 +195,7 @@ def big_shoe_rebounds
   game_hash.each do |location, team_data|
     team_data[:players].each do |attribute|
       if attribute[:shoe] > s_size 
+        s_size
          
     end 
   end   
